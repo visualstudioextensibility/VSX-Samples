6e6705ad324a2b66d1b513dda2a204c97e4a1afb
carlos-quintero <carlosq@mvps.org>
2016-10-09 09:43:01 +0200

    Initial version

---
+++ CommandSolutionContextMenu/CommandSolutionContextMenu/Command1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace CommandSolutionContextMenu
+{
+   internal sealed class Command1
+   {
+      public const int CommandId = 0x0100;
+
+      public static readonly Guid CommandSet = new Guid("42d8467b-0102-4e3f-964c-dbedf18b2323");
+
+      private readonly Package package;
+
+      private Command1(Package package)
+      {
+         if (package == null)
+         {
+            throw new ArgumentNullException("package");
+         }
+
+         this.package = package;
+
+         OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+         if (commandService != null)
+         {
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+            commandService.AddCommand(menuItem);
+         }
+      }
+
+      public static Command1 Instance
+      {
+         get;
+         private set;
+      }
+
+      private IServiceProvider ServiceProvider
+      {
+         get
+         {
+            return this.package;
+         }
+      }
+
+      public static void Initialize(Package package)
+      {
+         Instance = new Command1(package);
+      }
+
+      private void MenuItemCallback(object sender, EventArgs e)
+      {
+         string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+         string title = "Command1";
+
+         // Show a message box to prove we were here
+         VsShellUtilities.ShowMessageBox(
+             this.ServiceProvider,
+             message,
+             title,
+             OLEMSGICON.OLEMSGICON_INFO,
+             OLEMSGBUTTON.OLEMSGBUTTON_OK,
+             OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+      }
+   }
+}
