b8b0c0a124ad91b4c8642f861c38881a47f1168c
carlos-quintero <carlosq@mvps.org>
2016-10-29 07:33:23 +0200

    Initial version

---
+++ ContextMenu/ContextMenu/Commands/ToolWindowCommand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ContextMenu
+{
+   internal sealed class ToolWindowCommand
+   {
+      // Warning!!: Keep these values in sync with the .vsct file
+      public const int CommandId = 0x0100;
+      public static readonly Guid CommandSet = new Guid("c1a3a312-e25a-4cd1-b557-011428323a99");
+
+      private readonly Package package;
+
+      private ToolWindowCommand(Package package)
+      {
+         if (package == null)
+         {
+            throw new ArgumentNullException("package");
+         }
+
+         this.package = package;
+
+         OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+         if (commandService != null)
+         {
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.ShowToolWindow, menuCommandID);
+            commandService.AddCommand(menuItem);
+         }
+      }
+
+      public static ToolWindowCommand Instance
+      {
+         get;
+         private set;
+      }
+
+      private IServiceProvider ServiceProvider
+      {
+         get
+         {
+            return this.package;
+         }
+      }
+
+      public static void Initialize(Package package)
+      {
+         Instance = new ToolWindowCommand(package);
+      }
+
+      private void ShowToolWindow(object sender, EventArgs e)
+      {
+         // Get the instance number 0 of this tool window. This window is single instance so this instance
+         // is actually the only one.
+         // The last flag is set to true so that if the tool window does not exists it will be created.
+         ToolWindowPane window = this.package.FindToolWindow(typeof(MyToolWindow), 0, true);
+         if ((null == window) || (null == window.Frame))
+         {
+            throw new NotSupportedException("Cannot create tool window");
+         }
+
+         IVsWindowFrame windowFrame = (IVsWindowFrame)window.Frame;
+         Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
+      }
+   }
+}
---
+++ ContextMenu/ContextMenu/ToolWindowControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.Shell.Interop;
+using System.Windows.Controls;
+
+namespace ContextMenu
+{
+   public partial class ToolWindowControl : UserControl
+   {
+      private VSPackageContextMenu m_package;
+
+      public ToolWindowControl()
+      {
+         this.InitializeComponent();
+      }
+
+      public void Initialize(VSPackageContextMenu package)
+      {
+         m_package = package;
+      }
+
+      private void frame_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+      {
+         // Warning!!: Keep these values in sync with the .vsct file
+         const string guidVSPackageContextMenuCmdSet = "c1a3a312-e25a-4cd1-b557-011428323a99";
+         const int MyContextMenuId = 0x0200;
+
+         System.IServiceProvider serviceProvider;
+         IVsUIShell uiShell;
+         System.Guid contextMenuGuid = new System.Guid(guidVSPackageContextMenuCmdSet);
+         System.Windows.Point relativePoint;
+         System.Windows.Point screenPoint;
+         POINTS point;
+         POINTS[] points;
+
+         if (e.ChangedButton == System.Windows.Input.MouseButton.Right)
+         {
+            serviceProvider = (System.IServiceProvider)m_package;
+            uiShell = serviceProvider.GetService(typeof(SVsUIShell)) as IVsUIShell;
+
+            if (uiShell != null)
+            {
+               relativePoint = e.GetPosition(this);
+               screenPoint = this.PointToScreen(relativePoint);
+
+               point = new POINTS();
+               point.x = (short)screenPoint.X;
+               point.y = (short)screenPoint.Y;
+
+               points = new[] { point };
+
+               // TODO: error handling
+               uiShell.ShowContextMenu(0, ref contextMenuGuid, MyContextMenuId, points, null);
+            }
+         }
+      }
+   }
+}
