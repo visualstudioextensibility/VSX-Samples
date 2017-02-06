19591bb3967fb5652e4f4890f9448f818e421a96
Carlos Quintero <carlosq@mztools.com>
2017-02-06 17:39:26 +0100

    Initial version of VSIXLocalizedPackage

---
+++ VSIXLocalizedPackage/README.md
@@ -0,0 +1,16 @@
+HOWTO: Localize a Visual Studio package and its VSIX
+====================================================
+
+This sample shows how to localize:
+
+* The .vsct files of the commands of a package
+* The .resx files of the package
+* The .vsix file
+
+References:
+
+Localizing Menu Commands
+(https://msdn.microsoft.com/en-us/library/ee943168.aspx)
+
+Localizing VSIX Packages
+(https://msdn.microsoft.com/en-us/library/dd997147.aspx)
---
+++ VSIXLocalizedPackage/VSIXLocalizedPackage/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VSIXLocalizedPackage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VSIXLocalizedPackage")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
